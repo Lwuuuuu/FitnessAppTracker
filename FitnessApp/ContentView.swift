3c338e6daa0efd0339dae8ebb7bb7a3b9ec20bdc
Lawrence Wu <larbear1337@gmail.com>
2020-10-30 16:32:40 -0400

    Initial Commit

---
+++ FitnessApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessApp
+//
+//  Created by Kelley Wu on 10/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
